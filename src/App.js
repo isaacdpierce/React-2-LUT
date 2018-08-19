5cc750c7ebc33edde607a584551d37521b6f59f4
Isaac Pierce <contact@isaacdpierce.com>
2018-08-19 14:40:13 -0700

    Create User Context with Provider and Consumer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component, Fragment, Context } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Toggle } from "Utilities";
-import { Modal } from "Elements";
+import React, { Component, Fragment, createContext } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Toggle } from 'Utilities';
+import { Modal } from 'Elements';
+import User from './User';
+import { UserContext } from './UserContext';
+
+class UserProvider extends Component {
+  state = {
+    id: '123',
+    name: 'Benjamin',
+    email: 'ben@jamin.com'
+  };
+
+  logout = () => {
+    this.setState({
+      id: null,
+      name: '',
+      email: ''
+    });
+  };
+  render() {
+    return (
+      <UserContext.Provider value={{ user: this.state, logout: this.logout }}>
+        {this.props.children}
+      </UserContext.Provider>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Toggle>
-          {({ on, toggle }) => (
-            <Fragment>
-              <button onClick={toggle}>Login</button>
-              <Modal on={on} toggle={toggle}>
-                <h1>Have yerself an eggroll</h1>
-              </Modal>
-            </Fragment>
-          )}
-        </Toggle>
-      </div>
+      <UserProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <User />
+          <Toggle>
+            {({ on, toggle }) => (
+              <Fragment>
+                <button onClick={toggle}>Login</button>
+                <Modal on={on} toggle={toggle}>
+                  <h1>Have yerself an eggroll</h1>
+                </Modal>
+              </Fragment>
+            )}
+          </Toggle>
+        </div>
+      </UserProvider>
     );
   }
 }
